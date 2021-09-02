d7152fbc196da2d2406d2d612ec0603328d962a3
luminousbinary <boolwrld@outlook.com>
2021-09-02 22:16:53 +0100

    fixes

--- store/views.py
+++ store/views.py
@@ -1,22 +1,23 @@
+# from django.core import paginator
 from django.shortcuts import render
 from .models import Order, Product 
 from django.core.paginator import Paginator
 # Create your views here.
 
 def home(request):
-    product_objects = Product.objects.all()
+    product_object = Product.objects.all()
 
     # serch 
     item_name = request.GET.get('item_name')
     if item_name != '' and item_name is not None:
-        product_objects = product_objects.filter(title__icontains = item_name)
+        product_object = product_object.filter(title__icontains = item_name)
 
     #paginator
-    paginator = Paginator(product_objects,1)
+    paginator = Paginator(product_object, 3)
     page = request.GET.get('page')
-    product_objects = paginator.get_page(page)
+    product_object = paginator.get_page(page)
 
-    return render(request, 'store/home.html', {'product_objects':product_objects})
+    return render(request, 'store/home.html', {'product_objects':product_object})
 
 def item(request, id):
     product_object = Product.objects.get(id=id)
