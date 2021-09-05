0f7a629a8e22172f2d6ada84897523f4f395b53c
luminousbinary <boolwrld@outlook.com>
2021-09-06 00:40:38 +0100

    DB Fixes

--- store/views.py
+++ store/views.py
@@ -12,7 +12,7 @@ def home(request):
         product_objects = product_objects.filter(title__icontains = item_name)
 
     #paginator
-    paginator = Paginator(product_objects, 3)
+    paginator = Paginator(product_objects,14)
     page = request.GET.get('page')
     product_objects = paginator.get_page(page)
 
